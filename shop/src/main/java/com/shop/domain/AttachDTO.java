5ba2667c6bae9497d7982d41d65890aad20b5bbb
DESKTOP-RSQID9I\audwls <kimmjk3@gmail.com>
2022-02-28 17:20:10 +0900

    파일첨부 도메인 클래스(DTO) 생성

---
+++ shop/src/main/java/com/shop/domain/AttachDTO.java
@@ -0,0 +1,16 @@
+package com.shop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AttachDTO {
+    Integer attachNumber; // 파일 번호
+    Integer productNumber; // 게시글번호
+    String attachOriginalName; // 원본 파일명
+    String attachSaveName; // 저장 파일명
+    Integer attachSize; // 파일 크기
+    String attachInsertDate; // 등록일
+    String attachDeleteDate; // 삭제일
+}
