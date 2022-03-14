367caa57cdabc32bd5e674dcb549eb58442f9c12
DESKTOP-RSQID9I\audwls <kimmjk3@gmail.com>
2022-03-14 10:19:37 +0900

    파일첨부 위한 컨트롤러 수정

--- shop/src/main/java/com/shop/controller/ProductController.java
+++ shop/src/main/java/com/shop/controller/ProductController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.shop.constant.Method;
 import com.shop.domain.ProductDTO;
@@ -33,8 +34,8 @@ public class ProductController extends UiUtils {
     }
 
     @PostMapping(value = "/shop/productregister.do")
-    public String registerProduct(final ProductDTO params, Model model, HttpServletRequest request,
-            HttpSession session) {
+    public String registerProduct(final ProductDTO params, final MultipartFile[] files, Model model,
+            HttpServletRequest request, HttpSession session) {
         try {
             // 세션값 userID params 입력
             String userID = null;
@@ -43,7 +44,7 @@ public class ProductController extends UiUtils {
             }
             params.setUserID(userID);
 
-            boolean isRegistered = productService.registerProduct(params);
+            boolean isRegistered = productService.registerProduct(params, files);
             if (isRegistered == false) {
                 return showMessageWithRedirect("제품 등록에 실패하였습니다.", "/shop/productmanagement.do", Method.GET, null,
                         model);
@@ -76,28 +77,6 @@ public class ProductController extends UiUtils {
         return "shop/productregistration";
     }
 
-    /*
-     * @PostMapping(value = "/shop/register.do") public String registerBoard(final
-     * BoardDTO params, Model model, HttpServletRequest request, HttpSession
-     * session) { try {
-     * 
-     * // 세션값 userID params 입력 String userID = null; if
-     * (session.getAttribute("userID") != null) { userID = (String)
-     * session.getAttribute("userID"); } params.setUserID(userID);
-     * 
-     * boolean isRegistered = boardService.registerBoard(params); if (isRegistered
-     * == false) { return showMessageWithRedirect("게시글 등록에 실패하였습니다.",
-     * "/shop/list.do", Method.GET, null, model); } } catch (DataAccessException e)
-     * { return showMessageWithRedirect("데이터베이스 처리 과정에 문제가 발생하였습니다.",
-     * "/shop/list.do", Method.GET, null, model);
-     * 
-     * } catch (Exception e) { return showMessageWithRedirect("시스템에 문제가 발생하였습니다.",
-     * "/shop/list.do", Method.GET, null, model); }
-     * 
-     * return showMessageWithRedirect("게시글 등록이 완료되었습니다.", "/shop/list.do",
-     * Method.GET, null, model); }
-     */
-
     @GetMapping(value = "/shop/productview.do")
     public String openProductDetail(@RequestParam(value = "productNumber", required = false) Integer productNumber,
             Model model) {
@@ -115,25 +94,4 @@ public class ProductController extends UiUtils {
 
         return "shop/productview";
     }
-
-    /*
-     * @PostMapping(value = "/shop/delete.do") public String
-     * deleteBoard(@RequestParam(value = "postNumber", required = false) Integer
-     * postNumber, Model model) { System.out.println("/shop/delete.do 실행됨");
-     * 
-     * if (postNumber == null) { return showMessageWithRedirect("올바르지 않은 접근입니다.",
-     * "/shop/list.do", Method.GET, null, model); }
-     * 
-     * try { boolean isDeleted = boardService.deleteBoard(postNumber); if (isDeleted
-     * == false) { return showMessageWithRedirect("게시글 삭제에 실패하였습니다.",
-     * "/shop/list.do", Method.GET, null, model); } } catch (DataAccessException e)
-     * { return showMessageWithRedirect("데이터베이스 처리 과정에 문제가 발생하였습니다.",
-     * "/shop/list.do", Method.GET, null, model);
-     * 
-     * } catch (Exception e) { return showMessageWithRedirect("시스템에 문제가 발생하였습니다.",
-     * "/shop/list.do", Method.GET, null, model); }
-     * 
-     * return showMessageWithRedirect("게시글 삭제가 완료되었습니다.", "/shop/list.do",
-     * Method.GET, null, model); }
-     */
 }
