4c9dd2a58372b7e908e5e22dbbdfe2044a03313f
DESKTOP-RSQID9I\audwls <kimmjk3@gmail.com>
2022-04-23 14:58:52 +0900

    경로 권한 수정

--- shop/src/main/java/com/shop/controller/UserController.java
+++ shop/src/main/java/com/shop/controller/UserController.java
@@ -26,7 +26,7 @@ public class UserController extends UiUtils {
     private UserService userService;
 
     // 메인페이지 진입
-    @GetMapping(value = "/")
+    @GetMapping(value = "/shop/index.do")
     public String openMain(Model model) {
         System.out.println("index페이지 진입");
         return "shop/index";
@@ -36,26 +36,34 @@ public class UserController extends UiUtils {
     @GetMapping(value = "/shop/notice.do")
     public String openNotice(Model model) {
         System.out.println("공지사항 페이지 진입");
-        return showMessageWithRedirect("페이지 준비중입니다.", "/", Method.GET, null, model);
+        return showMessageWithRedirect("페이지 준비중입니다.", "/shop/index.do", Method.GET, null, model);
     }
 
     // 마이페이지 진입
     @GetMapping(value = "/shop/mypage.do")
     public String openMypage(Model model) {
         System.out.println("마이페이지 진입");
-        return showMessageWithRedirect("페이지 준비중입니다.", "/", Method.GET, null, model);
+        return showMessageWithRedirect("페이지 준비중입니다.", "/shop/index.do", Method.GET, null, model);
     }
 
     // 회원가입 페이지 진입
     @GetMapping(value = "/shop/join.do") // 회원가입 주소
-    public String openUserJoin(Model model) {
+    public String openUserJoin(Model model, HttpSession session) {
         System.out.println("회원가입 페이지 진입");
+
+        // 로그인이 되어있을 경우 접근 불가
+        String userID = (String) session.getAttribute("userID");
+        if (userID != null) {
+            return showMessageWithRedirect("잘못된 접근입니다.", "/shop/index.do", Method.GET, null, model);
+        }
+
         model.addAttribute("user", new UserDTO());
+
         return "shop/join";
     }
 
     // 회원가입 처리
-    @PostMapping(value = "/shop/join.do")
+    @PostMapping(value = "/shop/joinAction.do")
     public String registerUser(UserDTO params, Model model) {
         try {
             System.out.println("접속테스트");
@@ -78,16 +86,24 @@ public class UserController extends UiUtils {
         return showMessageWithRedirect("회원가입 성공하였습니다.", "/shop/login.do", Method.GET, null, model);
     }
 
-    // 로그인 주소 진입
+    // 로그인 페이지 진입
     @GetMapping(value = "/shop/login.do") // 로그인 주소
-    public String openUserLogin(Model model) {
-        model.addAttribute("user", new UserDTO());
+    public String openUserLogin(Model model, HttpSession session) {
         System.out.println("로그인페이지 진입");
+
+        // 로그인이 되어있을 경우 접근 불가
+        String userID = (String) session.getAttribute("userID");
+        if (userID != null) {
+            return showMessageWithRedirect("잘못된 접근입니다.", "/shop/index.do", Method.GET, null, model);
+        }
+
+        model.addAttribute("user", new UserDTO());
+
         return "shop/login";
     }
 
     // 로그인 처리
-    @PostMapping(value = "/shop/login.do")
+    @PostMapping(value = "/shop/loginAction.do")
     public String loginPOST(HttpServletRequest request, UserDTO params, HttpSession session, Model model) {
         // System.out.println("로그인 메서드 진입");
         // System.out.println("전달된 데이터:" + params);
@@ -98,8 +114,8 @@ public class UserController extends UiUtils {
 
         if (user == null) { // 일치하지 않은 아이디와 비밀번호 입력 할 경우
             session.setAttribute("userID", null);
-            return showMessageWithRedirect("아이디 또는 비밀번호를 잘못 입력했습니다.\n입력하신 내용을 다시 확인해주세요.", "/", Method.GET, null,
-                    model);
+            return showMessageWithRedirect("아이디 또는 비밀번호를 잘못 입력했습니다.\n입력하신 내용을 다시 확인해주세요.", "/shop/login.do", Method.GET,
+                    null, model);
         } else {
             // 일치하는 아이디, 비밀번호 입력 할 경우 로그인 성공 세션값 부여
             session.setAttribute("userID", user.getUserID());
@@ -107,15 +123,15 @@ public class UserController extends UiUtils {
             session.setAttribute("userAuthority", user.getUserAuthority());
         }
 
-        return "redirect:/";
+        return "redirect:/shop/index.do";
     }
 
     // 로그아웃
-    @GetMapping(value = "/shop/logout") // 회원가입 주소
+    @GetMapping(value = "/shop/logoutAction.do") // 회원가입 주소
     public String logout(HttpSession session) {
         // 세션제거
         session.invalidate();
-        return "redirect:/";
+        return "redirect:/shop/index.do";
     }
 
     // 관심상품 등록
@@ -165,6 +181,7 @@ public class UserController extends UiUtils {
         return "shop/interestitemlist";
     }
 
+    // 관심상품 삭제
     @GetMapping(value = "/shop/interestitemdelete.do")
     public String deleteBoard(@RequestParam(value = "productNumber", required = false) Integer productNumber,
             Model model, HttpSession session) {
