eff1f0994eaf2ab9c641429a8f0eb2ff4627c9a7
DESKTOP-9C7K9EN\kimmjk35 <kimmjk3@gmail.com>
2023-02-20 15:50:36 +0900

    lombok 없이 실행 getter/setter 추가

--- shop/src/main/java/com/shop/domain/ProductDTO.java
+++ shop/src/main/java/com/shop/domain/ProductDTO.java
@@ -22,8 +22,7 @@ public class ProductDTO {
 
     // Attach(첨부)
     Integer attachNumber; // 파일 번호
-
-    String attachThumbnailOriginalName; // 원본 파일명
+	String attachThumbnailOriginalName; // 원본 파일명
     String attachThumbnailSaveName; // 저장 파일명
     long attachThumbnailSize; // 파일 크기
     String attachThumbnailLocation; // 경로
@@ -35,4 +34,143 @@ public class ProductDTO {
 
     String attachInsertDate; // 등록일
     String attachDeleteDate; // 삭제일
+
+    public Integer getProductNumber() {
+		return productNumber;
+	}
+	public void setProductNumber(Integer productNumber) {
+		this.productNumber = productNumber;
+	}
+	public String getProductCategory() {
+		return productCategory;
+	}
+	public void setProductCategory(String productCategory) {
+		this.productCategory = productCategory;
+	}
+	public Integer getProductState() {
+		return productState;
+	}
+	public void setProductState(Integer productState) {
+		this.productState = productState;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public Integer getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(Integer productPrice) {
+		this.productPrice = productPrice;
+	}
+	public Integer getProductStock() {
+		return productStock;
+	}
+	public void setProductStock(Integer productStock) {
+		this.productStock = productStock;
+	}
+	public String getProductDesc() {
+		return productDesc;
+	}
+	public void setProductDesc(String productDesc) {
+		this.productDesc = productDesc;
+	}
+	public Integer getProductHits() {
+		return productHits;
+	}
+	public void setProductHits(Integer productHits) {
+		this.productHits = productHits;
+	}
+	public String getProductInputDate() {
+		return productInputDate;
+	}
+	public void setProductInputDate(String productInputDate) {
+		this.productInputDate = productInputDate;
+	}
+	public String getProductCurrent() {
+		return productCurrent;
+	}
+	public void setProductCurrent(String productCurrent) {
+		this.productCurrent = productCurrent;
+	}
+	public String getProductDeleteDate() {
+		return productDeleteDate;
+	}
+	public void setProductDeleteDate(String productDeleteDate) {
+		this.productDeleteDate = productDeleteDate;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public Integer getAttachNumber() {
+		return attachNumber;
+	}
+	public void setAttachNumber(Integer attachNumber) {
+		this.attachNumber = attachNumber;
+	}
+	public String getAttachThumbnailOriginalName() {
+		return attachThumbnailOriginalName;
+	}
+	public void setAttachThumbnailOriginalName(String attachThumbnailOriginalName) {
+		this.attachThumbnailOriginalName = attachThumbnailOriginalName;
+	}
+	public String getAttachThumbnailSaveName() {
+		return attachThumbnailSaveName;
+	}
+	public void setAttachThumbnailSaveName(String attachThumbnailSaveName) {
+		this.attachThumbnailSaveName = attachThumbnailSaveName;
+	}
+	public long getAttachThumbnailSize() {
+		return attachThumbnailSize;
+	}
+	public void setAttachThumbnailSize(long attachThumbnailSize) {
+		this.attachThumbnailSize = attachThumbnailSize;
+	}
+	public String getAttachThumbnailLocation() {
+		return attachThumbnailLocation;
+	}
+	public void setAttachThumbnailLocation(String attachThumbnailLocation) {
+		this.attachThumbnailLocation = attachThumbnailLocation;
+	}
+	public String getAttachContentsOriginalName() {
+		return attachContentsOriginalName;
+	}
+	public void setAttachContentsOriginalName(String attachContentsOriginalName) {
+		this.attachContentsOriginalName = attachContentsOriginalName;
+	}
+	public String getAttachContentsSaveName() {
+		return attachContentsSaveName;
+	}
+	public void setAttachContentsSaveName(String attachContentsSaveName) {
+		this.attachContentsSaveName = attachContentsSaveName;
+	}
+	public long getAttachContentsSize() {
+		return attachContentsSize;
+	}
+	public void setAttachContentsSize(long attachContentsSize) {
+		this.attachContentsSize = attachContentsSize;
+	}
+	public String getAttachContentsLocation() {
+		return attachContentsLocation;
+	}
+	public void setAttachContentsLocation(String attachContentsLocation) {
+		this.attachContentsLocation = attachContentsLocation;
+	}
+	public String getAttachInsertDate() {
+		return attachInsertDate;
+	}
+	public void setAttachInsertDate(String attachInsertDate) {
+		this.attachInsertDate = attachInsertDate;
+	}
+	public String getAttachDeleteDate() {
+		return attachDeleteDate;
+	}
+	public void setAttachDeleteDate(String attachDeleteDate) {
+		this.attachDeleteDate = attachDeleteDate;
+	}
 }
